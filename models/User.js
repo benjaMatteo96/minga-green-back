304ff3b0d26d8acc9b15a66a46cb0ce34d59ce9d
benjaMatteo96 <127891765+benjaMatteo96@users.noreply.github.com>
2023-09-02 11:08:46 -0400

    models para user

--- models/User.js
+++ models/User.js
@@ -1,11 +1,16 @@
 import {Schema,model,Types }from "mongoose";
-/* sheham define estrucutra de documentos */
+/* schema define estrucutra de documentos */
 /* modelo basado en la estructura*/
-/* types sirve para trabajar con tispo de datos especiales como objectId */
+/* types sirve para trabajar con tipos de datos especiales como objectId */
 
 let collection = 'users'
 let schema = new Schema({
-  name:{type:String, required:true}
+  name:{type:String, required:true},
+  email:{type:String, required:true},
+  passsword:{type:String, required:true},
+  photo:{type:String, required:true},
+  role:{type:Number, default:0 },
+  online: {type:String, required:false}
 }, {
   timestamps:true /* fecha de documentos */
 })
