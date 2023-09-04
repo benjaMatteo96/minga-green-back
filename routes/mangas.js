cd4c30b58344a038b26187e67a5f2aa6c71c69a7
bellavergara <bellaiivanavergara@gmail.com>
2023-09-04 20:58:28 -0300

    ejercicio endpoint, controlador para leer un capitulo

--- routes/mangas.js
+++ routes/mangas.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import getAllMangas from '../controllers/mangas/read.js';
-import checkUserAuthentication from '../utils/authentication';
 const router = express.Router();
 
 /* GET users listing. */
@@ -9,13 +8,3 @@ router.get('/', getAllMangas);
 export default router;
 
 
-router.get('/mangas/:page', (req, res) => {
-    const isAuthenticated = checkUserAuthentication(req);
-    if (isAuthenticated) {
-        // Renderizar la página de Mangas     r
-        es.render('mangas');
-    } else {
-        // Redirigir al usuario no autenticado a la página de NotAllowed     
-        res.redirect('/notallowed');
-    }
-});
