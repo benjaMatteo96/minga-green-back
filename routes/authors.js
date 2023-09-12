13ace239c2eacc592012bf5975ae83cca9a37309
Guadalupe-Alvarado <dai.22121994@gmail.com>
2023-09-12 17:42:26 -0300

    que se yo

--- routes/authors.js
+++ routes/authors.js
@@ -2,7 +2,7 @@ import express  from 'express';
 import readAuthors from '../controllers/authors/read_me.js';
 import read from '../controllers/authors/read.js';
 import createOneAuthor from '../controllers/authors/create.js' //crear
-import validadorAuthor from '../schemaValidator/validatorAuthor.js'
+import validadorAuthor from '../schema/validatorAuthor.js'
 import validator from '../middleware/validator.js';
 import passport from '../middleware/passport.js';
 import hasPermission from '../middleware/has_permition.js'
@@ -16,3 +16,6 @@ router.post('/create',  passport.authenticate('jwt', {session: false}), validato
 
 
 export default  router;
+
+/* border bottom 
+outline*/
