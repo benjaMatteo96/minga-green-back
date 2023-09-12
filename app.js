87b0b5ae7834538bd1d909a522d019a52629c33c
bellavergara <bellaiivanavergara@gmail.com>
2023-09-12 18:35:26 -0300

    e01-finalizado

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+//// Importacion los middlewares
+import errorHandler from './middleware/error-handler.js'
+import notFoundHandler from './middleware/not-found-handler.js'
+
 
 import indexRouter from './routes/index.js';
 /* import usersRouter from './routes/users.js'; */
 
-import{__dirname}from './utils.js'
-import{__filename}from './utils.js'
+import { __dirname } from './utils.js'
+import { __filename } from './utils.js'
 
 const app = express();
 
@@ -31,12 +35,19 @@ app.use('/', indexRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
+// e01-errors//
+
+// Agrega los middlewares a la aplicación
+app.use(errorHandler);
+app.use(notFoundHandler);
+//
+
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
