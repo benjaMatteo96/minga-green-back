80dcb80828b76230f52cf0534c2396e42e1acd9e
sofia <m.sofia.a.campos@gmail.com>
2023-09-14 15:03:39 -0300

    back sofi terminado

--- routes/chapters.js
+++ routes/chapters.js
@@ -1,9 +1,18 @@
 import express from 'express';
 import readOneChapters from '../controllers/chapters/read_one.js '
 import getPaginatedChapters from '../controllers/chapters/read.js'
+import createChapters from '../controllers/chapters/create.js';
+import addCoverPhoto from '../middlewares/add_cover_photo.js';
+import validator from '../middlewares/validator.js';
+import chapterValidator from '../validators/chapterValidator.js';
+import passport from '../middlewares/passport.js';
+
+
 const router = express.Router();
 
 router.get('/', getPaginatedChapters);
 router.get('/:id', readOneChapters);
+router.post('/',  passport.authenticate('jwt', {session:false}), addCoverPhoto, validator(chapterValidator),createChapters)
+
 
 export default router
