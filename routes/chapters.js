132d08e6b37bed6f68edea85c1341357f8c5c4ad
sofia <m.sofia.a.campos@gmail.com>
2023-09-16 14:38:08 -0300

    back terminado

--- routes/chapters.js
+++ routes/chapters.js
@@ -2,10 +2,10 @@ import express from 'express';
 import readOneChapters from '../controllers/chapters/read_one.js '
 import getPaginatedChapters from '../controllers/chapters/read.js'
 import createChapters from '../controllers/chapters/create.js';
-import addCoverPhoto from '../middlewares/add_cover_photo.js';
-import validator from '../middlewares/validator.js';
+import addCoverPhoto from '../middleware/add_cover_photo.js';
+import validator from '../middleware/validator.js';
 import chapterValidator from '../validators/chapterValidator.js';
-import passport from '../middlewares/passport.js';
+import passport from '../middleware/passport.js';
 
 
 const router = express.Router();
